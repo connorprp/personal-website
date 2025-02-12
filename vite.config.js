1c2927defe2d9218a38e09b4bdbb98dc758d4322
connorprp <connorpoindexter12@gmail.com>
2025-02-12 16:05:28 -0500

    hopefully fixed links

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+    base: "/client-side-demos/",
     build: {
         rollupOptions: {
             input: {
