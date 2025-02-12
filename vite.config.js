f22de5d7ed8eb805749fc7aef5bf7e5a02ae8281
connorprp <connorpoindexter12@gmail.com>
2025-02-12 15:27:46 -0500

    fixing relative links again

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 freeCodeCamp: resolve(__dirname, "freeCodeCamp/index.html"),
-                freeCodeCampStyles: resolve(__dirname, "freeCodeCamp/styles.css"),
-                freeCodeCampScript: resolve(__dirname, "freeCodeCamp/script.js"),
                 connorsWebsite: resolve(__dirname, "connorsWebsite/index.html"),
                 thirdPage: resolve(__dirname, "thirdPage/index.html"),
             }
