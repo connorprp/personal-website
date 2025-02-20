41f06ba730b58917d7800d7a3e452539d7008b87
connorprp <connorpoindexter12@gmail.com>
2025-02-19 21:52:35 -0500

    adding consistent nav bar and configured vite to build in docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path";
 export default defineConfig({
     base: "/client-side-demos/",
     build: {
+        outDir: "docs",
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
