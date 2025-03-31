bc735620016622fa773b7852b9f5d3081e7e2780
connorprp <connorpoindexter12@gmail.com>
2025-03-31 17:25:43 -0400

    featured web app init and changed global nav to be more user friendly

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig({
                 palindrome: resolve(__dirname, "palindrome/index.html"),
                 certification2: resolve(__dirname, "certification2/index.html"),
                 tournamentPayouts: resolve(__dirname, "tournamentPayouts/index.html"),
-                certification3: resolve(__dirname, "certification3/index.html")
+                certification3: resolve(__dirname, "certification3/index.html"),
+                photoSite: resolve(__dirname, "photoSite/index.html"),
+                about: resolve(__dirname, "photoSite/about/index.html"),
+                portfolio: resolve(__dirname, "photoSite/portfolio/index.html"),
+                events: resolve(__dirname, "photoSite/events/index.html"),
             }
         }
     }
