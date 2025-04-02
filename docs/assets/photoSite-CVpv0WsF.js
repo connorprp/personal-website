4f7e50d4ed65dca630368a7d13eb2ff33ba08b1f
connorprp <connorpoindexter12@gmail.com>
2025-04-02 14:00:09 -0400

    added global nav css files

--- docs/assets/photoSite-CVpv0WsF.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const o=document.getElementById("view-portfolio");o.addEventListener("click",()=>{window.location.href="./portfolio/"});
---
+++ docs/assets/photoSite-F3PsQNkU.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css                *//* empty css            */const o=document.getElementById("view-portfolio");o.addEventListener("click",()=>{window.location.href="./portfolio/"});
