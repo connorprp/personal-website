c600f073b8432836b59dd20d18ccdd66b13128f6
connorprp <connorpoindexter12@gmail.com>
2025-05-27 19:45:57 -0400

    fixing css linking

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    base: "/personal-site/",
+    base: "/personal-website/",
     build: {
         outDir: "docs",
         rollupOptions: {
